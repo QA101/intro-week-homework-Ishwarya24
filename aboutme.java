3a76d3413430767c695e8e3076846237020be248
Ishwarya24 <ashovarya.kp@gmail.com>
2017-10-08 11:42:43 -0700

    my first homework

--- aboutme.java
+++ aboutme.java
@@ -1,4 +1,19 @@
+package Homework1;
 
 public class aboutme {
+	
+		public static void main (String[] args)
+		{
+			System.out.print("My name ");
+			System.out.println("is Ishwarya");			
+			System.out.println("I work as an Enterprise Test Analyst");
+			System.out.println("I have started learning automation with selenium");
+			System.out.println("Learning automation will open a wide variety of opportunities");
+			System.out.println("I will be using java with selenium");
+			System.out.println("Selenium is a freeware");
+			System.out.println("Hence it is used by a lot of companies");
+		}
+		
+	
 
 }
